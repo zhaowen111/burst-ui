2b919ac46bcbb7950ed795e7e9c11486730ead3c
zhaowen <2872186458@qq.com>
2023-08-31 12:06:28 +0800

    创建项目

---
+++ packages/components/cycle-scroll/index.tsx
@@ -0,0 +1,5 @@
+function CycleScroll() {
+  return <div></div>;
+}
+
+export default CycleScroll;
---
+++ src/Example.tsx
@@ -0,0 +1,10 @@
+import CircleScroll from "../packages/components/cycle-scroll";
+function Example() {
+  return (
+    <div className="example">
+      <CircleScroll></CircleScroll>
+    </div>
+  );
+}
+
+export default Example;
